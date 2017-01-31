381003026aba1faa690259026ed61fb96dca7bb9
Patchra Rojanadickun <patchra07@hotmail.com>
2017-01-31 17:54:41 +0700

    Remove proxy

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '6RX4/OOatXA2HDqT0puXT7XunQffhOr9MMihxJSfRmFtlo6YJ1UPzGnQLbm4bTVH2FbZrmKjZrXVpQbxb4QiNNJaaYmrnlnJW1bBuENIuLuyZgV4w1TPj3FVoKrQx/+jNvnxrVW0baDmRGdj65B7sgdB04t89/1O/w1cDnyilFU=';
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:UlDg2fnqFYAnYaf';
+// $proxy = 'velodrome.usefixie.com:80';
+// $proxyauth = 'fixie:UlDg2fnqFYAnYaf';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -39,8 +39,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      curl_setopt($ch, CURLOPT_PROXY, $proxy);
-      curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+      // curl_setopt($ch, CURLOPT_PROXY, $proxy);
+      // curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
